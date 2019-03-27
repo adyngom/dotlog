5a349c0e64c51c37a305e3e384165cb396b436f7
adyngom <ady.touba.ngom@gmail.com>
2019-03-27 08:49:54 -0400

    * Test set will return an intuitive status to indicate success or failure. It will be collapsed by default and if one or more tests fail, the line will be red and green otherwise. It will also show the count of how many failed and how many paased. When opened, each line will show the color of the test status * The prinTable method ultimately uses the console.table to show the structure of an array or object. A limit, range or showOnly properties can be passed as parameters to get a subset of the result

--- consoleTest.js
+++ consoleTest.js
@@ -1,15 +1,42 @@
 var consoleTest = ( function() {
 
     function predicate(testrule = false, testlabel = 'test label:') {
-        return ( testrule ) ? console.log(`%c${testlabel}`, 'color: green') 
-                            : console.log(`%c${testlabel}`, 'color: red; font-weight: bold;');
+        return ( testrule ) ? console.log(`%c${testlabel}`, 'color: green') : console.log(`%c${testlabel}`, 'color: red; font-weight: bold;');
     }
 
     return {
-        describe(testDescription = 'test description:', testSet = []) {
-            console.group(`%c${testDescription}`, 'color: silver');
-                testSet.forEach( test => predicate(test.rule, test.label));
-            console.groupEnd();
+        describe(desc = 'test description:', testSet = []) {
+
+			let color = 'green', fontWeight = 'normal', passed = 0, failed = 0;
+
+			const testDisplay = testSet.map( test => {
+				if ( !test.rule ) {
+					color = 'red';
+					fontWeight = 'bold';
+					failed++;	
+				} else {
+					passed++;
+				}
+				return { color, fontWeight, label: test.label};
+			});
+
+			console.groupCollapsed(`%c${desc}: ${passed} passed - ${failed} failed`, `color: ${color}; font-weight: bold`);
+				testDisplay.forEach( td => console.log(`%c${td.label}`, `color: ${td.color}; font-weight: ${td.fontWeight};`));
+			console.groupEnd();
+            
+        },
+        printTable(arr = [], options = { limit: null, range: [], showOnly: []}) {
+        	let {limit, range, showOnly} = options;
+        	let _arr;
+			if(!!limit && typeof limit === 'number' && limit < arr.length) {
+				_arr = arr.slice(0, limit);
+			} else if(!!range && range.length >= 2) {
+				_arr = arr.slice(range[0], range[1])
+			} else {
+				_arr = arr;
+			}
+
+			console.table(_arr, showOnly);
         }
     };
 }());
